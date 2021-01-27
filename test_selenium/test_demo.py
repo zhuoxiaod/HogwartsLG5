fbd14ba2e884eeecbc5757314183ad2468c890ff
zhuoxiaodong <zhuoxiaod@126.com>
2021-01-27 11:55:14 +0800

    修改获取cookie方法

--- test_selenium/test_demo.py
+++ test_selenium/test_demo.py
@@ -3,6 +3,7 @@ from time import sleep
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class TestBaidu():
@@ -12,30 +13,32 @@ class TestBaidu():
     def teardown_method(self):
         self.driver.quit()
 
-
-    def test_cookie(self):
-    #     # 获取cookie
-    #     cookies = self.driver.get_cookies()
-    #     # 以文件流形式，打开文件
-    #     with open("cookie.json", "w") as f:
-    #     # 存储cookie到cookie.json
-    #         json.dump(cookies, f)
-        # 以文件流形式打开文件
+    def test_weixin(self):
         self.driver.get("https://work.weixin.qq.com/")
-        with open("cookie.json", "r") as f:
-        # 读取cookie
-            cookies = json.load(f)
-        for cookie in cookies:
-            self.driver.add_cookie(cookie)
-        self.driver.get("https://work.weixin.qq.com/wework_admin/frame#index")
         sleep(3)
-        self.driver.find_element(By.XPATH, "//*[@id='menu_customer']").click()
-        sleep(5)
+        self.driver.find_element(By.XPATH, "//*[@class='index_top_operation_loginBtn']").click()
+        sleep(8)
 
-    # def test_weixin(self):
-    #     self.driver.get("https://work.weixin.qq.com/")
-    #     sleep(3)
-    #     self.driver.find_element(By.XPATH, "//*[@class='index_top_operation_loginBtn']").click()
-    #     sleep(8)
+        cookies = self.driver.get_cookies()
+        # 以文件流形式，打开文件
+        with open("cookie.json", "w") as f:
+            # 存储cookie到cookie.json
+            json.dump(cookies, f)
 
-        self.driver.close()
+    # def test_cookie(self):
+        # 获取cookie
+
+        # 以文件流形式打开文件
+        # self.driver.get("https://work.weixin.qq.com/")
+        # with open("cookie.json", "r") as f:
+        # # 读取cookie
+        #     cookies = json.load(f)
+        # for cookie in cookies:
+        #     self.driver.add_cookie(cookie)
+        # self.driver.get("https://work.weixin.qq.com/wework_admin/frame#index")
+        # sleep(3)
+        # self.driver.find_element(By.XPATH, "//*[@id='menu_customer']").click()
+        # sleep(5)
+        # WebDriverWait(self.driver, 10).until()
+
+
